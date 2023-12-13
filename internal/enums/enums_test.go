0b61714af2df00280809c1e8562183944f4377a0
Will Taylor <willgeorgetaylor@gmail.com>
2023-12-13 09:29:37 +0000

    test: enums test

---
+++ internal/enums/enums_test.go
@@ -0,0 +1,27 @@
+package enums
+
+import (
+	"testing"
+)
+
+func TestAggregateOperationValues(t *testing.T) {
+	expectedValues := map[string]AggregateOperation{
+		"mean":   AggregateOperationMean,
+		"mode":   AggregateOperationMode,
+		"median": AggregateOperationMedian,
+		"min":    AgregateOperationMin,
+		"max":    AggregateOperationMax,
+		"sum":    AggregateOperationSum,
+	}
+
+	for key, expectedValue := range expectedValues {
+		actualValue, ok := AggregateOperationValues[key]
+		if !ok {
+			t.Errorf("Expected key '%s' not found in AggregateOperationValues", key)
+		}
+
+		if actualValue != expectedValue {
+			t.Errorf("Expected value '%d' for key '%s', got '%d'", expectedValue, key, actualValue)
+		}
+	}
+}
