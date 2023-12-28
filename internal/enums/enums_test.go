bf831d10d767d361f5a792154b09eea4cafc63ee
Will Taylor <willgeorgetaylor@gmail.com>
2023-12-28 14:08:42 +0000

    test: add test coverage for enum helper functions

--- internal/enums/enums_test.go
+++ internal/enums/enums_test.go
@@ -1,6 +1,7 @@
 package enums
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -23,5 +24,45 @@ func TestAggregateOperationValues(t *testing.T) {
 		if actualValue != expectedValue {
 			t.Errorf("Expected value '%d' for key '%s', got '%d'", expectedValue, key, actualValue)
 		}
+	}
+}
+
+func TestGetTestSuiteFields(t *testing.T) {
+	expectedFields := []string{"filepath", "name", "name+filepath"}
+
+	actualFields := GetTestSuiteFields()
+
+	if !reflect.DeepEqual(actualFields, expectedFields) {
+		t.Errorf("Expected fields %v, but got %v", expectedFields, actualFields)
+	}
+}
+
+func TestGetTestCaseFields(t *testing.T) {
+	expectedFields := []string{"classname", "file", "name"}
+
+	actualFields := GetTestCaseFields()
+
+	if !reflect.DeepEqual(actualFields, expectedFields) {
+		t.Errorf("Expected fields %v, but got %v", expectedFields, actualFields)
+	}
+}
+
+func TestGetAggregateOperations(t *testing.T) {
+	expectedOperations := []string{"max", "mean", "median", "min", "mode", "sum"}
+
+	actualOperations := GetAggregateOperations()
+
+	if !reflect.DeepEqual(actualOperations, expectedOperations) {
+		t.Errorf("Expected operations %v, but got %v", expectedOperations, actualOperations)
+	}
+}
+
+func TestGetRoundingModes(t *testing.T) {
+	expectedModes := []string{"ceil", "floor", "round"}
+
+	actualModes := GetRoundingModes()
+
+	if !reflect.DeepEqual(actualModes, expectedModes) {
+		t.Errorf("Expected modes %v, but got %v", expectedModes, actualModes)
 	}
 }
